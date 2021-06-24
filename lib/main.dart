e3b5131dba0f809d46a3e1397bd07e5e6f5c15e4
Franreno <franreno4@gmail.com>
2021-06-24 10:45:09 -0300

    Added main.dart schematics

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:zenith_monitor/constants/colors_constants.dart';
+import 'package:zenith_monitor/widgets/nossosValores.dart';
 
+void main() {
+  runApp(const ZenithMonitor());
+}
+
+class ZenithMonitor extends StatelessWidget {
+  const ZenithMonitor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Pagina(),
+    );
+  }
+}
+
+class Pagina extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              NossosValores(),
+            ],
+          ),
+        ),
+        backgroundColor: raisingBlack);
+  }
+}
