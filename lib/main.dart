f681c602b441ac676bc6e6d49d6d0b3bb4833f79
Franreno <franreno4@gmail.com>
2021-06-24 21:02:46 -0300

    Figure label and Component Title Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:zenith_monitor/constants/colors_constants.dart';
 import 'package:zenith_monitor/widgets/nossosValores.dart';
 
 void main() {
--- lib/widgets/nossosValores.dart
+++ lib/widgets/nossosValores.dart
@@ -1,6 +1,5 @@
 // To-Do:
-//        Add phrase under figure
-//        Add top bar with 'Nossos valores' and return button.
+//        Fix Alignment of everything
 
 import 'package:flutter/material.dart';
 import 'package:zenith_monitor/constants/colors_constants.dart';
@@ -8,7 +7,7 @@ import 'package:zenith_monitor/constants/colors_constants.dart';
 class NossosValores extends StatelessWidget {
   NossosValores({Key? key});
 
-  Widget nossosValoresContainer(String text, String pathToImg) {
+  Widget nossosValoresContainer(String text, String pathToImg, String nomeImg) {
     return Container(
       width: 340,
       height: 94,
@@ -20,15 +19,28 @@ class NossosValores extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            child: Image.asset(
-              pathToImg,
-              color: null,
-              fit: BoxFit.cover,
-              width: 37.0,
-              height: 38.11,
-              colorBlendMode: BlendMode.dstATop,
+            padding: EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  child: Image.asset(
+                    pathToImg,
+                    color: null,
+                    fit: BoxFit.cover,
+                    width: 37.0,
+                    height: 38.11,
+                    colorBlendMode: BlendMode.dstATop,
+                  ),
+                ),
+                Container(
+                  child: Text(
+                    nomeImg,
+                    style: TextStyle(color: white, fontSize: 11),
+                  ),
+                ),
+              ],
             ),
-            padding: EdgeInsets.all(10),
           ),
           Container(
             height: 49,
@@ -54,19 +66,46 @@ class NossosValores extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Spacer(flex: 2),
+              Spacer(),
+              Container(
+                width: double.infinity,
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                        padding: EdgeInsets.all(25),
+                        child: Image.asset(
+                          "assets/images/botaoVoltar.png",
+                          color: white,
+                          fit: BoxFit.cover,
+                          width: 12,
+                          height: 12,
+                        )),
+                    Container(
+                      child: Text(
+                        "Nossos Valores",
+                        style: TextStyle(color: white, fontSize: 18),
+                      ),
+                    )
+                  ],
+                ),
+              ),
               nossosValoresContainer(
                   "Incentivar a produção científica nacional\n e facilitar o acesso à ciência no\n ambiente universitário",
-                  "assets/images/figGroup.png"),
+                  "assets/images/figScientist.png",
+                  "Ciência"),
               Spacer(flex: 1),
               nossosValoresContainer(
-                  "O zenith possui uma hierarquia horizontal,\n onde todos os integrantes possuem voz,\n mas responsabilidades distintas",
-                  "assets/images/figProfessional.png"),
+                  "O zenith possui uma hierarquia\n horizontal, onde todos os integrantes\n possuem voz, mas responsabilidades\n distintas",
+                  "assets/images/figGroup.png",
+                  "Pessoas"),
               Spacer(flex: 1),
               nossosValoresContainer(
                   "Os integrantes tem oportunidades de\n desenvolver capacitações técnicas\n em grupo, praticando suas soft skills.",
-                  "assets/images/figScientist.png"),
-              Spacer(flex: 8),
+                  "assets/images/figProfessional.png",
+                  "Habilidade"),
+              Spacer(flex: 12),
             ],
           ),
         ),
