d35cf2b84295c8247472c60ed0448d1bcbb0df0b
VitorFCM <vitor.carreramiguel@gmail.com>
2021-07-05 21:18:36 -0300

    Adding basic widget file

---
+++ lib/widgets/mission_creation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MissionCreation extends StatefulWidget {
+  const MissionCreation({Key? key}) : super(key: key);
+
+  @override
+  _MissionCreationState createState() => _MissionCreationState();
+}
+
+class _MissionCreationState extends State<MissionCreation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
