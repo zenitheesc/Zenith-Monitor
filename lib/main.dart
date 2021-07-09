43fcaa289fa1ce964200a6c0bd5a10d87173fa46
trizcard <beatrizco@usp.br>
2021-07-09 14:04:57 -0300

    "esqueci minha senha" widget done

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:zenith_monitor/widgets/esqueci-minha-senhaContainer.dart';
 
+void main() {
+  runApp(const ZenithMonitor());
+}
+
+class ZenithMonitor extends StatelessWidget {
+  const ZenithMonitor({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Pagina(),
+    );
+  }
+}
+
+class Pagina extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return esqueciSenha();
+  }
+}
