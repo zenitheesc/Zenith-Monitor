8acfbe77b61858645794cb4658d2c728ed3d625d
g-mafra <g.mafra.costa@gmail.com>
2021-07-12 12:06:58 -0300

    Adding small containers

--- lib/widgets/login.dart
+++ lib/widgets/login.dart
@@ -6,7 +6,7 @@ class LoginZenithMonitor extends StatelessWidget {
 
   Widget loginContainersName(String texto) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 4.5),
+      padding: const EdgeInsets.symmetric(horizontal: 4.5,vertical: 2.5),
       child: Container(
         width: 167,
         height: 62,
@@ -14,21 +14,21 @@ class LoginZenithMonitor extends StatelessWidget {
           borderRadius: BorderRadius.circular(15),
           color: Colors.black,
         ),
-              child: Padding(
+        child: Padding(
           padding: const EdgeInsets.all(18),
-          child: Text(texto,
-          textAlign: TextAlign.left,
-          style: TextStyle(color: gray, fontSize: 18, fontFamily: 'DMSans'),
+          child: Text(
+            texto,
+            textAlign: TextAlign.left,
+            style: TextStyle(color: gray, fontSize: 18, fontFamily: 'DMSans'),
           ),
         ),
       ),
-      
     );
   }
 
   Widget loginContainersGeneral(String texto) {
     return Padding(
-      padding: const EdgeInsets.all(5.0),
+      padding: const EdgeInsets.all(2.5),
       child: Container(
         width: 339,
         height: 62,
@@ -38,9 +38,10 @@ class LoginZenithMonitor extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.all(18),
-          child: Text(texto,
-          textAlign: TextAlign.left,
-          style: TextStyle(color: gray, fontSize: 18, fontFamily: 'DMSans'),
+          child: Text(
+            texto,
+            textAlign: TextAlign.left,
+            style: TextStyle(color: gray, fontSize: 18, fontFamily: 'DMSans'),
           ),
         ),
       ),
@@ -74,33 +75,66 @@ class LoginZenithMonitor extends StatelessWidget {
         backgroundColor: eerieBlack);
   }
 
-  Widget buildMainBody() {
+  Widget smallContainer(String text, int textColor) {
     return Center(
       child: Container(
-        width: 395,
-        height: 434,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(60),gradient: LinearGradient(begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        stops: [0.5,1],
-        colors: [raisingBlack,Colors.black]),
+        width: 173,
+        height: 39,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+            color: Color(textColor),
         ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  loginContainersName('Nome'),
-                  loginContainersName('Sobrenome')
-                ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(text,
+          textAlign: TextAlign.center,
+          style: TextStyle(color: white, fontSize: 18, fontFamily: 'DMSans'),
+      ),
+        ),
+    ),);
+  }
+
+  Widget buildMainBody() {
+    return GridTile(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: Container(
+              width: 395,
+              height: 434,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60),
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.5, 1],
+                    colors: [raisingBlack, Colors.black]),
               ),
-              loginContainersGeneral('Email'),
-              loginContainersGeneral('Senha'),
-              loginContainersGeneral('Confirmar senha')
-            ],
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        loginContainersName('Nome'),
+                        loginContainersName('Sobrenome')
+                      ],
+                    ),
+                    loginContainersGeneral('Email'),
+                    loginContainersGeneral('Senha'),
+                    loginContainersGeneral('Confirmar senha')
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [smallContainer('Voltar',0XFFE57373),smallContainer('Submeter',0XFF8BC34A)],
+          )
+        ],
       ),
     );
   }
