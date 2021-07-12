009774e6c0a40faf9ecdf921f97f6f0880cb6e6e
igorlourencetti <igorlourencetti@usp.br>
2021-07-11 22:10:16 -0300

    TextFields and button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:zenith_monitor/constants/colors_constants.dart';
 import 'package:zenith_monitor/widgets/mission_creation.dart';
 
 void main(){
@@ -20,13 +22,21 @@ class Application extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criação de Missão'),
+        backgroundColor: eerieBlack,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: gray)
+      ),
       body: Center(
         child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            MissionCreation()
+            MissionCreation(),
           ],
         ),
       ),
+      backgroundColor: eerieBlack,
     );
   }
 }
--- lib/widgets/mission_creation.dart
+++ lib/widgets/mission_creation.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+import 'package:zenith_monitor/constants/colors_constants.dart';
 import 'package:flutter/material.dart';
 
 class MissionCreation extends StatefulWidget {
@@ -10,12 +12,60 @@ class MissionCreation extends StatefulWidget {
 class _MissionCreationState extends State<MissionCreation> {
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Insira algo novo'
-        ),
-      obscureText: true,
+    return Container(
+      width: 1080.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              style: TextStyle(color: white,),
+              decoration: InputDecoration(
+                labelStyle: TextStyle(color: gray),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: gray),
+                  borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: gray),
+                  borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
+                labelText: 'Inserir novo campo',
+              ),
+            ),
+          ),  
+          SizedBox(width: 20,),
+          Expanded(
+            child: TextField(
+              style: TextStyle(color: white),
+              decoration: InputDecoration(
+                labelStyle: TextStyle(color: gray),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: gray),
+                  borderRadius: BorderRadius.all(Radius.circular(10))
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: gray),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                labelText: 'Tipo',
+              ),
+            ),
+          ),
+          SizedBox(width: 20),
+          Expanded(
+            child: Row(
+              children: <Widget>[FloatingActionButton(
+                  onPressed: (){
+                    // onPressed code
+                  },
+                  child: const Icon(Icons.add, color: eerieBlack,),
+                  backgroundColor: gray,
+                )
+              ],
+            ),
+          )
+        ]
+      ),
     );
   }
 }
