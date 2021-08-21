6a572b44cc5667ea6ed3cfa80e85763faabdb9a6
VitorFCM <vitor.carreramiguel@gmail.com>
2021-08-21 13:12:08 -0300

    Adding basic configuration for communication with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:zenith_monitor/utils/ui/animations/zenith_progress_indicator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ZenithMonitor());
 }
 
@@ -9,17 +12,35 @@ class ZenithMonitor extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Application(),
     );
   }
 }
 
 class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
-
+  Application({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return const Scaffold(backgroundColor: Colors.red);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const Scaffold(backgroundColor: Colors.green);
+        }
+
+        return const ZenithProgressIndicator(
+            size: 30, fileName: "z_icon_white.png");
+        // Otherwise, show something whilst waiting for initialization to complete
+      },
+    );
   }
 }
