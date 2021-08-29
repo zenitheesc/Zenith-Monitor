585edf34c73a9b54d69367856e51106cd0ffa782
VitorFCM <vitor.carreramiguel@gmail.com>
2021-08-29 13:11:43 -0300

    Removing pipelines folder from lib root

--- lib/pipelines/mission_pipeline/mission_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'mission_bloc.dart';
-
-abstract class MissionEvent {}
-
-class SetVariablesListEvent extends MissionEvent {
-  MissionVariablesList variablesList;
-
-  SetVariablesListEvent({required this.variablesList});
-}
--- lib/pipelines/mission_pipeline/mission_state.dart
+++
@@ -1,5 +0,0 @@
-part of 'mission_bloc.dart';
-
-abstract class MissionState {}
-
-class MissionStateInitial extends MissionState {}
