7a9e96ad85c7b8ba6e1e43a17bd0388b4494d283
Franreno <franreno4@gmail.com>
2022-01-27 16:26:42 -0300

    Starting plots generator

---
+++ lib/utils/ui/plots/plot_gen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+
+
+class PlotGenerator extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
+
+
+
+
+
