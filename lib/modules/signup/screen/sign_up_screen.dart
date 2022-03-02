cbc6b67e94b7e0f675fe9eded6cac1dede10adfa
VitorFCM <vitor.carreramiguel@gmail.com>
2022-03-02 02:47:42 -0300

    Deleting signup module and renaming register module to signup
    The signup module was created in the branch components/signup, but the register module was already being used for signup stuff

--- lib/modules/signup/screen/sign_up_screen.dart
+++ lib/modules/signup/screen/sign_up_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zenith_monitor/modules/register/bloc/sign_up_bloc.dart';
+import 'package:zenith_monitor/modules/signup/screen/signup_screen.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
@@ -9,7 +10,7 @@ class SignUpScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [BlocProvider(create: (context) => SignUpBloc())],
-      child: const Scaffold(),
+      child: const SignUpWidget(),
     );
   }
 }
