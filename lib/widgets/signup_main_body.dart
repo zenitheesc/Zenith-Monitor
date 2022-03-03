47a7f913708007a409480d23b9761a874f8065e0
VitorFCM <vitor.carreramiguel@gmail.com>
2022-03-03 17:10:19 -0300

    Changing SignUpMainBody from a stateless widget to stateful widget
    This way, the contents of the TextFields stop disappearing every time the keyboard goes down

--- lib/widgets/signup_main_body.dart
+++ lib/widgets/signup_main_body.dart
@@ -6,7 +6,7 @@ import 'package:zenith_monitor/widgets/signup_buttons_section.dart';
 import 'package:zenith_monitor/widgets/signup_name_password_section.dart';
 import 'package:zenith_monitor/widgets/signup_profile_image_section.dart';
 
-class SignUpMainBody extends StatelessWidget {
+class SignUpMainBody extends StatefulWidget {
   final double screenWidth;
   final double screenHeight;
   final Orientation deviceOrientation;
@@ -15,11 +15,39 @@ class SignUpMainBody extends StatelessWidget {
       {required this.screenWidth,
       required this.screenHeight,
       required this.deviceOrientation});
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController surnameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
-  final TextEditingController pwdConfirmController = TextEditingController();
+
+  @override
+  _SUPMainBodyState createState() => _SUPMainBodyState();
+}
+
+class _SUPMainBodyState extends State<SignUpMainBody> {
+  late TextEditingController nameController;
+  late TextEditingController surnameController;
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+  late TextEditingController pwdConfirmController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    nameController = TextEditingController();
+    surnameController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    pwdConfirmController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    nameController.dispose();
+    surnameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    pwdConfirmController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,11 +57,12 @@ class SignUpMainBody extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             ProfileImageSection(
-                screenWidth: screenWidth, screenHeight: screenHeight),
+                screenWidth: widget.screenWidth,
+                screenHeight: widget.screenHeight),
             NamePasswordSection(
-              screenWidth: screenWidth,
-              screenHeight: screenHeight,
-              deviceOrientation: deviceOrientation,
+              screenWidth: widget.screenWidth,
+              screenHeight: widget.screenHeight,
+              deviceOrientation: widget.deviceOrientation,
               nameController: nameController,
               surnameController: surnameController,
               emailController: emailController,
@@ -41,9 +70,9 @@ class SignUpMainBody extends StatelessWidget {
               pwdConfirmController: pwdConfirmController,
             ),
             ButtonsSection(
-              screenWidth: screenWidth,
-              screenHeight: screenHeight,
-              deviceOrientation: deviceOrientation,
+              screenWidth: widget.screenWidth,
+              screenHeight: widget.screenHeight,
+              deviceOrientation: widget.deviceOrientation,
               funcConfirm: () => BlocProvider.of<SignUpBloc>(context).add(
                   UserRegisterEvent(
                       newUser: LocalUser(nameController.text,
