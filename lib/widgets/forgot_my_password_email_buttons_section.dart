9f72296e4d7dcb717191ca356a2d102557a0c807
VitorFCM <vitor.carreramiguel@gmail.com>
2022-03-03 20:13:33 -0300

    Creating TextField controller and resolving the same problem of the last commit

--- lib/widgets/forgot_my_password_email_buttons_section.dart
+++ lib/widgets/forgot_my_password_email_buttons_section.dart
@@ -7,40 +7,47 @@ class EmailButtonsSection extends StatelessWidget {
   final double screenWidth;
   final double screenHeight;
   final Orientation deviceOrientation;
+  final TextEditingController emailController;
 
   const EmailButtonsSection(
       {required this.screenWidth,
       required this.screenHeight,
-      required this.deviceOrientation});
+      required this.deviceOrientation,
+      required this.emailController});
 
   @override
   Widget build(BuildContext context) {
     return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
       TextFieldContainer(
+          controller: emailController,
           labelText: 'Email cadastrado',
           fontSize: _fontSize(),
           width: 0.80 * screenWidth,
           height: 0.09 * screenHeight,
           margin: EdgeInsets.only(bottom: 0.03 * screenHeight)),
       ElevatedButtonContainer(
-          labelText: 'Submeter',
-          textStyle: TextStyle(
-              color: Colors.black, fontSize: _fontSize(), fontFamily: 'DMSans'),
-          width: 0.62 * screenWidth,
-          height: 0.07 * screenHeight,
-          margin: EdgeInsets.only(
-              top: 0.03 * screenHeight, bottom: 0.02 * screenHeight),
-          borderRadius: 0.016 * screenHeight,
-          buttonColor: white),
+        labelText: 'Submeter',
+        textStyle: TextStyle(
+            color: Colors.black, fontSize: _fontSize(), fontFamily: 'DMSans'),
+        width: 0.62 * screenWidth,
+        height: 0.07 * screenHeight,
+        margin: EdgeInsets.only(
+            top: 0.03 * screenHeight, bottom: 0.02 * screenHeight),
+        borderRadius: 0.016 * screenHeight,
+        buttonColor: white,
+        buttonFunction: () => Navigator.pop(context),
+      ),
       ElevatedButtonContainer(
-          labelText: 'Voltar',
-          textStyle: TextStyle(
-              color: white, fontSize: _fontSize(), fontFamily: 'DMSans'),
-          width: 0.62 * screenWidth,
-          height: 0.06 * screenHeight,
-          margin: EdgeInsets.only(top: 0.02 * screenHeight),
-          borderRadius: 0.03 * screenHeight,
-          buttonColor: lightCoral)
+        labelText: 'Voltar',
+        textStyle: TextStyle(
+            color: white, fontSize: _fontSize(), fontFamily: 'DMSans'),
+        width: 0.62 * screenWidth,
+        height: 0.06 * screenHeight,
+        margin: EdgeInsets.only(top: 0.02 * screenHeight),
+        borderRadius: 0.03 * screenHeight,
+        buttonColor: lightCoral,
+        buttonFunction: () => Navigator.pop(context),
+      )
     ]);
   }
 
