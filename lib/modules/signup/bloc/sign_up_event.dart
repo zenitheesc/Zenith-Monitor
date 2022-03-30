e3415c4cfa32741734a77ff88190a0587df0dfd5
VitorFCM <vitor.carreramiguel@gmail.com>
2022-03-30 17:56:31 -0300

    Adding the signup bloc logic

--- lib/modules/signup/bloc/sign_up_event.dart
+++ lib/modules/signup/bloc/sign_up_event.dart
@@ -5,6 +5,15 @@ abstract class SignUpEvent {}
 class UserRegisterEvent extends SignUpEvent {
   LocalUser newUser;
   String password;
+  String pwdConfirmation;
 
-  UserRegisterEvent({required this.newUser, required this.password});
+  UserRegisterEvent(
+      {required this.newUser,
+      required this.password,
+      required this.pwdConfirmation});
+}
+
+class PickImageEvent extends SignUpEvent {
+  ImageSource source;
+  PickImageEvent({required this.source});
 }
