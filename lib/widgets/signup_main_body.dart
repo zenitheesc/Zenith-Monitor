48f3a0c728cc82e4f1d202b1899f592e3d1acacd
VitorFCM <vitor.carreramiguel@gmail.com>
2022-03-30 18:47:57 -0300

    Changing signup screen components to handle image selection and bloc interaction

--- lib/widgets/signup_main_body.dart
+++ lib/widgets/signup_main_body.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zenith_monitor/constants/colors_constants.dart';
 import 'package:zenith_monitor/modules/signup/bloc/sign_up_bloc.dart';
 import 'package:zenith_monitor/utils/mixins/class_local_user.dart';
+import 'package:zenith_monitor/utils/ui/animations/zenith_progress_indicator.dart';
 import 'package:zenith_monitor/widgets/signup_buttons_section.dart';
 import 'package:zenith_monitor/widgets/signup_name_password_section.dart';
 import 'package:zenith_monitor/widgets/signup_profile_image_section.dart';
+import 'package:zenith_monitor/widgets/status_message.dart';
 
 class SignUpMainBody extends StatefulWidget {
   final double screenWidth;
@@ -49,39 +52,74 @@ class _SUPMainBodyState extends State<SignUpMainBody> {
     pwdConfirmController.dispose();
   }
 
+  Widget statusMessage(SignUpState state) {
+    if (state is SuccessfulSignup) {
+      return const StatusMessage(
+        message:
+            "Sign up realizado com sucesso! Basta confirmar o email e realizar o login",
+        color: mantisGreen,
+      );
+    } else if (state is SignUpError) {
+      return StatusMessage(message: state.errorMessage, color: lightCoral);
+    }
+    return const SizedBox.shrink();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ProfileImageSection(
-                screenWidth: widget.screenWidth,
-                screenHeight: widget.screenHeight),
-            NamePasswordSection(
-              screenWidth: widget.screenWidth,
-              screenHeight: widget.screenHeight,
-              deviceOrientation: widget.deviceOrientation,
-              nameController: nameController,
-              surnameController: surnameController,
-              emailController: emailController,
-              passwordController: passwordController,
-              pwdConfirmController: pwdConfirmController,
+    return BlocBuilder<SignUpBloc, SignUpState>(
+      builder: (context, state) {
+        if (state is LoadingState) {
+          return const ZenithProgressIndicator(
+            size: 100,
+            fileName: "z_icon_white.png",
+          );
+        }
+        if (state is SuccessfulSignup) {
+          nameController.clear();
+          surnameController.clear();
+          emailController.clear();
+          passwordController.clear();
+          pwdConfirmController.clear();
+        }
+        return Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ProfileImageSection(
+                    screenWidth: widget.screenWidth,
+                    screenHeight: widget.screenHeight),
+                NamePasswordSection(
+                  screenWidth: widget.screenWidth,
+                  screenHeight: widget.screenHeight,
+                  deviceOrientation: widget.deviceOrientation,
+                  nameController: nameController,
+                  surnameController: surnameController,
+                  emailController: emailController,
+                  passwordController: passwordController,
+                  pwdConfirmController: pwdConfirmController,
+                ),
+                Padding(
+                    padding: const EdgeInsets.only(top: 40),
+                    child:
+                        Container(height: 40.0, child: statusMessage(state))),
+                ButtonsSection(
+                  screenWidth: widget.screenWidth,
+                  screenHeight: widget.screenHeight,
+                  deviceOrientation: widget.deviceOrientation,
+                  funcConfirm: () => BlocProvider.of<SignUpBloc>(context).add(
+                      UserRegisterEvent(
+                          newUser: LocalUser(nameController.text,
+                              surnameController.text, emailController.text),
+                          password: passwordController.text,
+                          pwdConfirmation: pwdConfirmController.text)),
+                )
+              ],
             ),
-            ButtonsSection(
-              screenWidth: widget.screenWidth,
-              screenHeight: widget.screenHeight,
-              deviceOrientation: widget.deviceOrientation,
-              funcConfirm: () => BlocProvider.of<SignUpBloc>(context).add(
-                  UserRegisterEvent(
-                      newUser: LocalUser(nameController.text,
-                          surnameController.text, null, emailController.text),
-                      password: passwordController.text)),
-            )
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
