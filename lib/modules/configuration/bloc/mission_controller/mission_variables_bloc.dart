f03591718208471e15476b82a4595fa70f94ccf9
Leonardo Celente <leocelente@gmail.com>
2023-02-26 11:02:59 -0300

    Handle BLE connection feedback on configuration screen

--- lib/modules/configuration/bloc/mission_controller/mission_variables_bloc.dart
+++ lib/modules/configuration/bloc/mission_controller/mission_variables_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:typed_data';
 import 'package:bloc/bloc.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
@@ -7,7 +6,6 @@ import 'package:zenith_monitor/utils/mixins/mission_variables/class_mission_vari
 import 'package:zenith_monitor/utils/mixins/mission_variables/class_mission_variables.dart';
 import 'package:zenith_monitor/utils/mixins/mission_variables/mission_variables_exceptions.dart';
 import 'package:zenith_monitor/core/pipelines/data_pipeline/data_bloc.dart';
-import 'package:zenith_monitor/utils/services/bluetooth/bluetooth.dart';
 import 'package:zenith_monitor/utils/services/firestore_services/firestore_services.dart';
 import 'package:zenith_monitor/utils/services/firestore_services/firestore_services_exceptions.dart';
 
@@ -23,19 +21,16 @@ class MissionVariablesBloc
   MissionVariablesList variablesList;
   DataBloc dataBloc;
   FirestoreServices firestoreServices = FirestoreServices();
-  Bluetooth bluetoothService = Bluetooth();
   Map<String, bool> connections;
   final Connectivity _connectivity;
-  final Set<BluetoothDevice> _bluetoothDevices;
-  StreamSubscription<BluetoothDiscoveryResult>? _devicesStream;
 
   MissionVariablesBloc(this.variablesList, this.dataBloc)
       : connections = <String, bool>{},
         _connectivity = Connectivity(),
-        _bluetoothDevices = {},
         super(VariablesInitial()) {
     connections = {
-      "Dispositivo usb": dataBloc.usbIsConnected,
+      "Dispositivo USB": dataBloc.usbIsConnected,
+      "Dispositivo BT": dataBloc.btIsConnected,
       "Internet": false,
     };
 
@@ -48,14 +43,20 @@ class MissionVariablesBloc
 
     dataBloc.stream.listen((event) {
       if (event is UsbDisconnectedState) {
-        connections["Dispositivo usb"] = false;
+        connections["Dispositivo USB"] = false;
         add(ConnectionChanged());
       } else if (event is UsbConnectedState) {
-        connections["Dispositivo usb"] = true;
+        connections["Dispositivo USB"] = true;
+        add(ConnectionChanged());
+      } else if (event is BluetoothConnectedState) {
+        connections["Dispositivo BT"] = true;
+        add(ConnectionChanged());
+      } else if (event is BluetoothDisconnectedState) {
+        connections["Dispositivo BT"] = false;
         add(ConnectionChanged());
       }
     });
-    add(SearchBluetoothDevices());
+    // add(SearchBluetoothDevices());
   }
 
   @override
@@ -101,47 +102,6 @@ class MissionVariablesBloc
         } catch (e) {
           print(e);
         }
-      }
-    } else if (event is ConnectionChanged) {
-      yield NewConnectionsState(connections);
-    } else if (event is BluetoothDeviceDiscovered) {
-      yield NewBluetoothDevices(bluetoothDevices: _bluetoothDevices);
-    } else if (event is SearchBluetoothDevices) {
-      yield DiscoveringBluetoothDevices();
-      if (_devicesStream != null) {
-        await _devicesStream!.cancel();
-      }
-      _bluetoothDevices.clear();
-      add(BluetoothDeviceDiscovered());
-      _devicesStream = bluetoothService.getDevicesStream().listen((result) {
-        BluetoothDevice device = result.device;
-        if (_bluetoothDevices.add(device)) {
-          add(BluetoothDeviceDiscovered());
-        }
-      });
-      _devicesStream!.onDone(() {
-        add(DiscoveryFinished());
-      });
-    } else if (event is DiscoveryFinished) {
-      yield BluetoothDiscoveryFinished();
-    } else if (event is ConnectToDevice) {
-      BluetoothDevice device = _bluetoothDevices.elementAt(event.index - 1);
-
-      print("vai tentar conectar com o index: " +
-          event.index.toString() +
-          " " +
-          (device.name ?? device.address));
-      BluetoothConnection? connection =
-          await bluetoothService.connectToDevice(device);
-      if (connection == null) {
-        print("deu ruim a conexao");
-      } else if (connection.input != null) {
-        print("deu certo: " + connection.isConnected.toString());
-        connection.input!.listen((Uint8List data) {
-          print("pacote chegando: " + data.toString());
-        }).onDone(() {
-          print('Disconnected by remote request');
-        });
       }
     } else {
       print("Unknown event in Variables Bloc");
